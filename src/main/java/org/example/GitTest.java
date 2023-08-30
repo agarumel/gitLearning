15f4488bbc233a15ee66104536af621e8319470b
Aga Rumel <agarumel@gmail.com>
2023-08-30 11:27:05 -0400

    added gittwo

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -2,10 +2,9 @@ package org.example;
 
 public class GitTest {
     public static void main(String[] args) {
-            System.out.println("My name is Mafi");
+            System.out.println("My name is Rumel");
             System.out.println("My age is 34");
             System.out.println("I am a good boy!");
             System.out.println("I study hard!");
-
         }
     }
---
+++ src/main/java/org/example/GitTwo.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class GitTwo {
+    public static void main(String[] args) {
+        System.out.println("RPint this");
+    }
+}

9b4936055c17467799c5b97045d880fe940c538e
Aga Rumel <agarumel@gmail.com>
2023-08-30 13:50:00 -0400

    added new file

--- src/main/java/org/example/GitTwo.java
+++ src/main/java/org/example/GitTwo.java
@@ -4,5 +4,6 @@ public class GitTwo {
     public static void main(String[] args) {
         System.out.println("RPint this");
         System.out.println("Print again!");
+        System.out.println("Rumel");
     }
 }
